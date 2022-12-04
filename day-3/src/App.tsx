eb617d2db57ea3c0bbd970bb76a8fd0879afc368
Tomas Birbe <tomas.birbe@gmail.com>
2022-12-04 14:01:41 -0300

    main: add func to add gifts on the gift's list

--- day-3/src/App.tsx
+++ day-3/src/App.tsx
@@ -1,6 +1,22 @@
+import { useState } from "react";
 import styles from "./App.module.css";
 
+const icons = ["candy-cane", "gift-box", "mistletoe-flower", "snowflake", "star"];
+
+type Gift = String;
+
 function App() {
+  const [gifts, setGifts] = useState<Gift[]>([]);
+
+  function addGift(event: React.FormEvent) {
+    event.preventDefault();
+    const { new_gift } = event.target as HTMLFormElement;
+    const newGift = new_gift.value;
+
+    setGifts((prevState) => [...prevState, newGift]);
+    new_gift.value = "";
+  }
+
   return (
     <main className={styles.app}>
       <section>
@@ -12,29 +28,28 @@ function App() {
           </h1>
         </article>
         <article>
-          <form>
+          <form onSubmit={addGift}>
             <label htmlFor="new_gift">Agrega un regalo</label>
             <input id="new_gift" type="text" />
             <button type="submit">Agregar!</button>
           </form>
           <ul>
-            <li>
-              <img
-                alt=""
-                height="40px"
-                src="public/icons/christmas-mistletoe-flower.svg"
-                width="40px"
-              />
-              <p>Regalo</p>
-            </li>
-            <li>
-              <img alt="" height="40px" src="public/icons/christmas-snowflake.svg" width="40px" />
-              <p>Regalo</p>
-            </li>
-            <li>
-              <img alt="" height="40px" src="public/icons/christmas-star.svg" width="40px" />
-              <p>Regalo</p>
-            </li>
+            {gifts.length === 0 ? (
+              <p>Probá agregar un regalo!</p>
+            ) : (
+              gifts.map((gift) => (
+                <li key={Math.random() * 100 * new Date().getDate()}>
+                  <img
+                    aria-hidden
+                    alt="Icon"
+                    height="40px"
+                    src={`public/icons/${icons[1]}.svg`}
+                    width="40px"
+                  />
+                  <p>{gift}</p>
+                </li>
+              ))
+            )}
           </ul>
         </article>
       </section>
